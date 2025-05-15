48275f6a160e2bb43c20073dffc58a482bc5d30c
Rohit <140368952+Rohit367673@users.noreply.github.com>
2025-05-15 11:57:11 +0530

    Update Product.js
    Signed-off-by: Rohit <140368952+Rohit367673@users.noreply.github.com>

--- Frontend/react-app/src/component/Product.js
+++ Frontend/react-app/src/component/Product.js
@@ -34,7 +34,7 @@ const Product = () => {
       const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/products`, {
         params: {
           page,
-          limit: 20, 
+          limit: 15, 
           category: selectedCategory === "all" ? "All" : selectedCategory,
         },
       });
